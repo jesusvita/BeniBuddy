198bf3044138a16b626bd44fda0dd8f4c405ff07
Jvita <jesusvitaa@gmail.com>
2025-05-06 22:48:20 -0400

    add password reset

--- myproject/settings.py
+++ myproject/settings.py
@@ -45,13 +45,14 @@ ALLOWED_HOSTS = [host for host in ALLOWED_HOSTS if host]
 # Application definition
 
 INSTALLED_APPS = [
+    'myapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp',
+    
 ]
 
 MIDDLEWARE = [
